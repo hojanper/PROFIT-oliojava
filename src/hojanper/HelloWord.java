ad2fd54871101f7a5fae085f9d7927ca04bccc21
Marko Back <marko.back@tieto.com>
2015-01-14 11:51:54 +0200

    test

---
+++ src/hojanper/HelloWord.java
@@ -0,0 +1,13 @@
+package hojanper;
+
+public class HelloWord 
+{
+
+    public static void main(String[] args) 
+    {
+        // TODO Auto-generated method stub
+
+        System.out.println("HELOUUU MAAILMA");
+    }
+
+}
